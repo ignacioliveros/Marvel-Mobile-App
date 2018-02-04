ec665e0b8ce0fd2be3611db1287438185f0ff997
ignacioliveros <ignacioliveros@gmail.com>
2018-02-04 00:13:21 -0500

    add style with sass

--- app/hero/hero-detail/hero-detail.component.ts
+++ app/hero/hero-detail/hero-detail.component.ts
@@ -8,7 +8,8 @@ import { Hero } from '../hero-model/hero';
 @Component({
     moduleId: module.id,
     selector: 'ns-hero-detail',
-    templateUrl: './hero-detail.component.html'
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -25,13 +26,17 @@ export class HeroDetailComponent implements OnInit {
         this.pageRoute.activatedRoute
             .switchMap(activatedRoute => activatedRoute.params)
             .forEach((params) => { this.id = +params["id"]; });
+        this.isLoading = true;
         this.getHero(this.id);
+
     }
 
     getHero(id: number) {
         this.heroService.getHero(id)
             .subscribe(data => {
                 this.hero = data.hero;
+            }, error => console.log(error),
+            () => {
                 this.isLoading = false;
             });
     }
